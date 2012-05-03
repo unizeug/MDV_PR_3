416ac706ef83e8eae9a08bf4e2193d51700c4106
Dirk <coolice.f.b@web.de>
2012-05-03 19:59:09 +0200

    Protokoll + Bilder

--- MDV_PR_3/MDV PR 3/Matlab/antialasing_2.m
+++ MDV_PR_3/MDV PR 3/Matlab/antialasing_2.m
@@ -1,13 +1,7 @@
 
 
 
-% designs an order n lowpass digital Butterworth filter with normalized cutoff
-% frequency Wn. It returns the zeros and poles in length n column vectors z and p,
-% and the gain in the scalar k.
-%[z,p,k] = butter(2,3.1*(10^3));
 
-%butw = mkfilter(0.5,4,'butterw'); 
-b = mkfilter(10,2,'butterw')
+b = mkfilter(3100/(2*pi),8,'butterw')
 bode(b,'-');
 grid on;
-% megend('Butterworth')
